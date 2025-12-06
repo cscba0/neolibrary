aa72c1218e62e5c1c7485b6d136e7678e8d7f6d8
cscba0 <cscba01367@gmail.com>
2025-12-06 14:23:27 +0900

    fix(config): add config path

--- test/aplusb.test.cpp
+++ test/aplusb.test.cpp
@@ -2,7 +2,10 @@
 
 #include <string>
 
-#include "../all.hpp"
+#include "../fastio/base.hpp"
+#include "../fastio/signed/read.hpp"
+#include "../fastio/signed/write.hpp"
+#include "../fastio/string/write.hpp"
 #include "../templates/template.hpp"
 
 int main() {
